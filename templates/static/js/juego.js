b28a0827bb6959e0106198a23115334fdc5f69d4
Nando1999 <fjmasache@espe.edu.ec>
2022-06-27 22:42:23 -0500

    cambios en juego

--- templates/static/js/juego.js
+++ templates/static/js/juego.js
@@ -1,39 +1,37 @@
+        var canvasWidth = 600;
+        var canvasHeight = 400;
+        //Creación de la variable 
+        var player;
+        var playerYPosition = 200;
 
+        var fallSpeed = 0;
+        var interval = setInterval(updateCanvas, 20);
 
-var canvasWidth = 900;
-var canvasHeight = 500;
+        var isJumping = false;
+        var jumpSpeed = 0;
 
-var player;
-var playerYPosition = 200;
+        var block;
 
-var fallSpeed = 0;
-var interval = setInterval(updateCanvas, 20);
+        // Crear una puntuación de 0 para empezar
+        var score = 0;
+        // Crear una variable para contener nuestro scoreLabel
+        var scoreLabel;
 
-var isJumping = false;
-var jumpSpeed = 0;
-
-var block;
-
-// Create a score of 0 to start with
-var score = 0;
-// Create a variable to hold our scoreLabel
-var scoreLabel;
-
-function startGame() {
-    gameCanvas.start();
-    player = new createPlayer(30, 30, 10);
-    block = new createBlock();
-    // Assign your scoreLabel variable a value from scoreLabel()
-    scoreLabel = new createScoreLabel(10, 30);
+        function startGame() {
+            gameCanvas.start();
+            player = new createPlayer(30, 30, 10);
+            block = new createBlock();
+        // Asigne a su variable scoreLabel un valor de scoreLabel()
+        scoreLabel = new createScoreLabel(10, 30);
 }
 
-var gameCanvas = {
-    canvas: document.createElement("canvas"),
-    start: function() {
-        this.canvas.width = canvasWidth;
-        this.canvas.height = canvasHeight;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        var gameCanvas = {
+            canvas: document.createElement("canvas"),
+            start: function() {
+                this.canvas.width = canvasWidth;
+                this.canvas.height = canvasHeight;
+                this.context = this.canvas.getContext("2d");
+                document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
 }
 
@@ -45,8 +43,7 @@ function createPlayer(width, height, x) {
     
     this.draw = function() {
         ctx = gameCanvas.context;
-        ctx.fillStyle = lamp;
-        var img = document.getElementById("lamp")
+        ctx.fillStyle = "green";
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
     this.makeFall = function() {
@@ -80,7 +77,7 @@ function createBlock() {
     
     this.draw = function() {
         ctx = gameCanvas.context;
-        ctx.fillStyle = "blue";
+        ctx.fillStyle = "red";
         ctx.fillRect(this.x, this.y, width, height);
     }
     this.attackPlayer = function() {
@@ -94,7 +91,7 @@ function createBlock() {
             speed = randomNumber(4, 6);
             this.y = canvasHeight - height;
             this.x = canvasWidth;
-            // Increase your score if your block made it to the edge
+            // Aumenta tu puntuación si tu bloque llegó al borde
             score++;
         }
     }
@@ -125,7 +122,6 @@ function createScoreLabel(x, y) {
         ctx = gameCanvas.context;
         ctx.font = "25px Marker Felt";
         ctx.fillStyle = "black";
-        
         ctx.fillText(this.text, this.x, this.y);
     }
 }
@@ -143,7 +139,7 @@ function updateCanvas() {
     block.draw();
     block.attackPlayer();
     
-    // Redraw your score and update the value
+    // Vuelva a dibujar su puntaje y actualice el valor
     scoreLabel.text = "SCORE: " + score;
     scoreLabel.draw();
 }
